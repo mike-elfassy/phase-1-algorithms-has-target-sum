ca2d5884917c1482c3088a36b238e936fb97d6b6
Michael Elfassy <mike.elfassy@gmail.com>
2022-12-24 13:10:05 -0700

    commit tree 487c83f37f92af02e3ea1aa56544e9bc6477ca2d

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (i = 0; i < array.length; i++) {
+    for (j = 0; j < array.length; j++) {
+      if (i === j) {}
+      else if (target === array[i] + array[j]) {return true}
+    }
+    if (i === j) {}
+    else if (target === array[i] + array[j]) {return true}
+  }
+  return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,17 +18,26 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  
 */
 
 /*
   Add written explanation of your solution here
+  Filter & Sort the array
+  Add the first and last item, if greater than target, discard.
+  brute force it
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([3, 8, 12, 4, 10, 7], 10));
 
   console.log("");
 
